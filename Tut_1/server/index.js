68f01af1e0e293b5538ff4fa1da1ba31f75a4242
Noman-Nom <noman5456318@gmail.com>
2024-03-16 19:45:02 +0500

    dummy arrays

--- Tut_1/server/index.js
+++ Tut_1/server/index.js
@@ -1,1 +1,25 @@
-console.log("hello world from server/index.js")
+const express = require('express');
+const app = express();
+
+
+const user = [
+
+    {
+        id : 1,
+        name : 'Muhammmad Noman',
+        pasword : 'Noman1234',
+        isAdmin : true
+        
+    },
+
+    {
+        id : 2,
+        name : 'Muhammmad Anwar',
+        pasword : 'Anwar1234',
+        isAdmin : false
+
+    },
+]
+
+
+app.listen(5000,()=>console.log('Server Running'))
