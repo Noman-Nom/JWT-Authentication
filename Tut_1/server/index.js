6a37e1e1032da9ed31d5ec1fd61e8d7e4a418098
Noman-Nom <noman5456318@gmail.com>
2024-03-17 16:17:26 +0500

    admin delete api working completed

--- Tut_1/server/index.js
+++ Tut_1/server/index.js
@@ -1,25 +1,97 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+app.use(express.json())
 
 
-const user = [
+const users = [
 
     {
         id : 1,
-        name : 'Muhammmad Noman',
-        pasword : 'Noman1234',
+        username : 'Muhammad Noman',
+        password : 'noman1234',
         isAdmin : true
         
     },
 
     {
         id : 2,
-        name : 'Muhammmad Anwar',
-        pasword : 'Anwar1234',
+        username : 'Muhammmad Anwar',
+        password : 'Anwar1234',
         isAdmin : false
 
     },
 ]
+ 
+            app.post("/api/login",(req,res)=>{
+
+                const {username,password} = req.body;
+             const user = users.find((u)=>{
+
+             
+                return u.username === username && u.password === password;
 
 
+             })
+             if(user){
+                // res.json(user);
+                    const accessToken = jwt.sign({
+                        id: user.id,
+                        isAdmin : user.isAdmin
+                    },'mySecretKey');
+
+                    res.json({
+                        username : user.username,
+                        isAdmin : user.isAdmin,
+                        accessToken : accessToken
+                    
+                    });
+                 
+             } else {
+
+                res.status(400).json({message : "Invalid Username or Password"})
+             }
+
+                // res.json("Working")  for testing postman
+            })
+                
+            const verify = (req ,res , next)=>{
+
+                const authHeader = req.headers.authorization;
+
+                if(authHeader){
+
+                    const token = authHeader.split(" ")[1];
+
+                    jwt.verify(token,'mySecretKey',(err , user)=>{
+                        if(err){
+
+                            return res.status(403).json("Token is not valid")
+                        }
+                        req.user = user;
+                        next();
+
+                    })
+
+                }else{
+
+                    res.status(401).json({message : "You are not authenticated"})
+                }
+
+
+
+            }
+
+            app.delete("/api/users/:userId",verify,(req,res)=>{
+
+                if(req.user.id === req.params.userId || req.user.isAdmin){
+
+                    res.json("User has been deleted");
+
+                }else{
+
+                    res.status(403).json("You are not allowed to delete this user")
+                }
+            })
+ 
 app.listen(5000,()=>console.log('Server Running'))
